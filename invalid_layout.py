2215d464fe98b141dcbbb2f6a3d57bad5085396a
sarrvesh <sarrvesh.ss@gmail.com>
2020-06-16 16:58:37 +0200

    show error message for invalid url

---
+++ invalid_layout.py
@@ -0,0 +1,9 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+layout = dbc.Row(
+            html.H3('You are trying to access a page that does not exist!'), 
+            justify="center"
+         )
+
+#layout = html.H3('You are trying to access a page that does not exist!')
